cf8d4cd8ebfc5329a63ffebd88782439b0e4fbc4
wefat <we_fat@hotmail.com>
2026-06-21 18:17:19 +0700

    fix:setting vercel

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,10 @@ app.get("/health", (req, res) => {
   res.json({ status: "OK", timestamp: new Date() });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
